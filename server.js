0ce52d0cf9fc2a37e07465dbdba540a694c37b21
Ahmed Mahmoud <a.abdelfatah@itv-me.com>
2019-07-27 10:55:23 +0400

    get/post

--- server.js
+++ server.js
@@ -6,6 +6,26 @@ const PORT = process.env.PORT || config.development.server.port;
 const HOST = config.development.server.host;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//form
+app.get("/", (req, res, next) => {
+  res.send(`
+    <h1>Hello World!</h1>
+    <form action="/entry" method="post">
+    <div>
+    <label>Enter Your MSG</label>
+    <input type ="text" name="msg">    
+    <button type="submit">Enter Your Message</button>
+
+    </div>
+    </form>
+    `);
+});
+//receiving request
+app.post("/entry", (req, res, next) => {
+  console.log(`${req.body.msg}`);
+  res.send(req.body.msg);
+});
 //running server
 app.listen(PORT, () => {
   console.log(`Server is running ${HOST}:${PORT}`);
