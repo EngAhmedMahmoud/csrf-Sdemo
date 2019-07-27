d0050e77b8ba3afcd1e73e18cd70b3e9d6186f8d
Ahmed Mahmoud <a.abdelfatah@itv-me.com>
2019-07-27 11:27:41 +0400

    finilizing-protection

--- server.js
+++ server.js
@@ -4,27 +4,34 @@ const bodyParser = require("body-parser");
 const config = require("./config");
 const PORT = process.env.PORT || config.development.server.port;
 const HOST = config.development.server.host;
+const uuid = require("uuid/v1");
+const cookieParser = require("cookie-parser");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cookieParser());
 //form
 app.get("/", (req, res, next) => {
+  let token = uuid();
+  res.cookie("token", token);
   res.send(`
     <h1>Hello World!</h1>
     <form action="/entry" method="post">
     <div>
     <label>Enter Your MSG</label>
-    <input type ="text" name="msg">    
+    <input type ="text" name="msg">   
+    <input type="hidden" value="${token}" name="_csrf"> 
     <button type="submit">Enter Your Message</button>
-
     </div>
     </form>
     `);
 });
 //receiving request
 app.post("/entry", (req, res, next) => {
-  console.log(`${req.body.msg}`);
-  res.send(req.body.msg);
+  if (req.cookies.token === req.body._csrf) {
+    res.send(req.body.msg);
+  } else {
+    res.send("Unauthorized token");
+  }
 });
 //running server
 app.listen(PORT, () => {
